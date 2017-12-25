2b6ca33b52256e799f26b32364d1c9755eb3de94
Greg Pascale <greg.pascale@gmail.com>
2017-12-24 18:57:38 -0800

    make bootstrap script handle spaces properly

--- .scripts/bootstrap.sh
+++ .scripts/bootstrap.sh
@@ -1,4 +1,3 @@
-set -e
 git clone --bare https://github.com/gpascale/dots2.git $HOME/.cfg
 function cfg {
    git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
@@ -7,9 +6,17 @@ mkdir -p .config-backup
 cfg checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
-  else
-    echo "Backing up pre-existing dot files.";
-    cfg checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+else
+  echo "Backing up pre-existing dot files.";
+  echo "FUCK `cfg checkout 2>&1 | egrep "^\s+(\w|\.)"`"
+  cfg checkout 2>&1 | egrep "^\s+(\w|\.)" | awk '{$1=$1};1' | while read LINE; do
+    FILE=$LINE
+    DIR=`dirname "$FILE" | awk '{$1=$1};1'`
+    SRC="$FILE"
+    TARGET=".config-backup/$FILE"
+    mkdir -p ".config-backup/$DIR"
+    mv "$SRC" "$TARGET"
+  done  
 fi;
 cfg checkout
 cfg config status.showUntrackedFiles no
--- .scripts/install.sh
+++ .scripts/install.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env zsh
 
-# Copy files to home directory
-rsync --exclude "*.zsh-theme" --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" --exclude "*~" -avc --no-perms ./\.* ~
-
 # Copy over my custom ZSH theme
 ZSHDIR="$HOME/.oh-my-zsh/custom/themes"
 [ -d "$ZSHDIR" ] || mkdir $ZSHDIR
